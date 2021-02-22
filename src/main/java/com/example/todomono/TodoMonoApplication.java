eaed7b48cca950375b2d72c724f2865dacd2830b
gverissi <greg.verissimo@gmail.com>
2021-02-22 16:10:38 +0100

    First commit

---
+++ src/main/java/com/example/todomono/TodoMonoApplication.java
@@ -0,0 +1,13 @@
+package com.example.todomono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todomono/TodoMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todomono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
