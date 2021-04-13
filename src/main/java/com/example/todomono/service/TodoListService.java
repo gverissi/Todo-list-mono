ebc45cd2aec28440306e55e8b9e28681161dfb1a
gverissi <greg.verissimo@gmail.com>
2021-04-13 22:22:15 +0200

    Refactor TodoListService

--- src/main/java/com/example/todomono/service/TodoListService.java
+++ src/main/java/com/example/todomono/service/TodoListService.java
@@ -1,13 +1,11 @@
 package com.example.todomono.service;
 
-import com.example.todomono.dao.CustomerDaoInterface;
 import com.example.todomono.dao.TodoListDaoInterface;
 import com.example.todomono.entity.Customer;
 import com.example.todomono.entity.TodoList;
 import com.example.todomono.exception.EntityNotFoundException;
 import com.example.todomono.exception.TodoListAlreadyExistException;
 import com.example.todomono.form.TodoListForm;
-import com.example.todomono.security.AuthenticationFacadeInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,10 +22,6 @@ public class TodoListService {
         this.todoListDao = todoListDao;
     }
 
-    public List<TodoList> findAllByCustomer(Customer customer) {
-        return todoListDao.findAllByCustomer(customer);
-    }
-
     public TodoList createOneForCustomer(Customer customer, TodoListForm todoListForm) throws TodoListAlreadyExistException {
         String title = todoListForm.getTitle();
         if (todoListExists(customer, title)) throw new TodoListAlreadyExistException("There is already a todo-list with name: " + title + ".");
@@ -35,6 +29,10 @@ public class TodoListService {
         todoList.setCustomer(customer);
         todoList.setNum(todoListDao.countByCustomer(customer) + 1);
         return todoListDao.save(todoList);
+    }
+
+    public List<TodoList> findAllByCustomer(Customer customer) {
+        return todoListDao.findAllByCustomer(customer);
     }
 
     public TodoList getOneByCustomerAndNum(Customer customer, long todoListNum) {
