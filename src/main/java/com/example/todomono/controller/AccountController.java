a11ef127c884fbf4b3febd386c8928611602ff81
gverissi <greg.verissimo@gmail.com>
2021-04-25 21:20:55 +0200

    Add stats for account page

--- src/main/java/com/example/todomono/controller/AccountController.java
+++ src/main/java/com/example/todomono/controller/AccountController.java
@@ -1,10 +1,14 @@
 package com.example.todomono.controller;
 
+import com.example.todomono.entity.Todo;
+import com.example.todomono.entity.TodoList;
 import com.example.todomono.exception.EntityAlreadyExistException;
 import com.example.todomono.exception.WrongPasswordException;
 import com.example.todomono.form.CustomerChangeNameForm;
 import com.example.todomono.form.CustomerChangePasswordForm;
 import com.example.todomono.form.CustomerDeleteAccountForm;
+import com.example.todomono.service.TodoListService;
+import com.example.todomono.service.TodoService;
 import com.example.todomono.service.customer.AccountService;
 import com.example.todomono.service.customer.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +21,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class AccountController {
 
     private final HomeService homeService;
     private final AccountService accountService;
+    private final TodoListService todoListService;
+    private final TodoService todoService;
 
     @Autowired
-    public AccountController(HomeService homeService, AccountService accountService) {
+    public AccountController(HomeService homeService, AccountService accountService, TodoListService todoListService, TodoService todoService) {
         this.homeService = homeService;
         this.accountService = accountService;
+        this.todoListService = todoListService;
+        this.todoService = todoService;
     }
 
     @GetMapping("/account")
     public String showAccountPage(Model model) {
         model.addAttribute("title", "Account");
+        List<TodoList> todoLists = todoListService.findAllByCustomer(homeService.getCustomer());
+        long totalTodoLists = todoLists.size();
+        model.addAttribute("totalTodoLists", totalTodoLists);
+        long finishedTodoLists = todoListService.findAllByCustomer(homeService.getCustomer()).stream().filter(TodoList::isFinished).count();
+        model.addAttribute("finishedTodoLists", finishedTodoLists);
+        long totalTodos = todoLists.stream().mapToLong(todoList -> todoService.findAllByTodoList(todoList).size()).sum();
+        model.addAttribute("totalTodos", totalTodos);
+        long doneTodos = todoLists.stream().mapToLong(todoList -> todoService.findAllByTodoList(todoList).stream().filter(Todo::isDone).count()).sum();
+        model.addAttribute("doneTodos", doneTodos);
         return "account/account";
     }
 
