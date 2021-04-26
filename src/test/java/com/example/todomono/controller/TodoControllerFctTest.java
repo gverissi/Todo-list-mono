4d2b781ffc6d46ca6f05a38cfd59faffbb59d35d
gverissi <greg.verissimo@gmail.com>
2021-04-26 19:13:34 +0200

    Add TodoControllerFctTest

--- src/test/java/com/example/todomono/controller/TodoControllerFctTest.java
+++ src/test/java/com/example/todomono/controller/TodoControllerFctTest.java
@@ -1,8 +1,10 @@
 package com.example.todomono.controller;
 
 import com.example.todomono.dao.CustomerDaoInterface;
+import com.example.todomono.dao.TodoDaoInterface;
 import com.example.todomono.dao.TodoListDaoInterface;
 import com.example.todomono.entity.Customer;
+import com.example.todomono.entity.Todo;
 import com.example.todomono.entity.TodoList;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -26,10 +28,13 @@ import static org.springframework.web.util.UriUtils.encode;
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class TodoListControllerFctTest {
+class TodoControllerFctTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private TodoDaoInterface todoDao;
 
     @Autowired
     private TodoListDaoInterface todoListDao;
@@ -37,77 +42,82 @@ class TodoListControllerFctTest {
     @Autowired
     private CustomerDaoInterface customerDao;
 
-    private Customer customer;
+    private TodoList todoList;
 
     @BeforeAll
     @WithMockUser(roles = "USER", username = "greg")
     void init() {
-        customer = customerDao.findByName("greg");
-        TodoList todoList = new TodoList("My todo-list 1");
+        Customer customer = customerDao.findByName("greg");
+        todoList = new TodoList("My todo-list");
         todoList.setNum(1);
         todoList.setCustomer(customer);
         todoListDao.save(todoList);
-        todoList = new TodoList("My todo-list 2");
-        todoList.setNum(2);
-        todoList.setCustomer(customer);
-        todoListDao.save(todoList);
-        todoList = new TodoList("My todo-list 3");
-        todoList.setNum(3);
-        todoList.setCustomer(customer);
-        todoListDao.save(todoList);
+
+        Todo todo = new Todo("My todo 1");
+        todo.setNum(1);
+        todo.setTodoList(todoList);
+        todoDao.save(todo);
+        todo = new Todo("My todo 2");
+        todo.setNum(2);
+        todo.setTodoList(todoList);
+        todoDao.save(todo);
+        todo = new Todo("My todo 3");
+        todo.setNum(3);
+        todo.setTodoList(todoList);
+        todoDao.save(todo);
     }
 
     @Test
     @WithMockUser(roles = "USER", username = "greg")
-    void createATodoList() throws Exception {
-        String title = "My todo-list";
-        int nbTodoLists = todoListDao.countByCustomer(customer);
+    void createATodo() throws Exception {
+        String label = "My todo";
+        int nbTodos = todoDao.countByTodoList(todoList);
 
         mockMvc.perform(
-                post("/todo-lists")
+                post("/todo-lists/" + todoList.getNum() + "/todos")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .content(encode("title", "UTF-8") + "=" + encode(title, "UTF-8"))
+                        .content(encode("label", "UTF-8") + "=" + encode(label, "UTF-8"))
                         .with(csrf()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("todo-list/todo-list-collection"));
+                .andExpect(view().name("todo/todo-collection"));
 
-        TodoList todoList = todoListDao.findByCustomerAndTitle(customer, title);
-        assertEquals(title, todoList.getTitle());
-        List<TodoList> todoLists = todoListDao.findAllByCustomer(customer);
-        assertEquals(nbTodoLists + 1, todoLists.size());
+        Todo todo = todoDao.findByTodoListAndLabel(todoList, label);
+        assertEquals(label, todo.getLabel());
+        List<Todo> todos = todoDao.findAllByTodoList(todoList);
+        assertEquals(nbTodos + 1, todos.size());
     }
 
     @Test
     @WithMockUser(roles = "USER", username = "greg")
-    void updateATodoList() throws Exception {
-        String newTitle = "My new title";
-
+    void updateATodo() throws Exception {
+        String newLabel = "My new title";
         mockMvc.perform(
-                put("/todo-lists/1")
+                put("/todo-lists/" + todoList.getNum() + "/todos/1")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .content(encode("title", "UTF-8") + "=" + encode(newTitle, "UTF-8"))
+                        .content(encode("label", "UTF-8") + "=" + encode(newLabel, "UTF-8"))
                         .with(csrf()))
                 .andExpect(status().isFound())
-                .andExpect(view().name("redirect:/todo-lists"));
+                .andExpect(view().name("redirect:/todo-lists/{todoListNum}/todos"));
 
-        TodoList updatedTodoList = todoListDao.findByCustomerAndTitle(customer, newTitle);
-        assertEquals(newTitle, updatedTodoList.getTitle());
+        Todo updatedTodo = todoDao.findByTodoListAndLabel(todoList, newLabel);
+        assertEquals(newLabel, updatedTodo.getLabel());
+        List<Todo> todos = todoDao.findAllByTodoList(todoList);
     }
 
     @Test
     @WithMockUser(roles = "USER", username = "greg")
-    void deleteATodoList() throws Exception {
-        int nbTodoLists = todoListDao.countByCustomer(customer);
+    void deleteATodo() throws Exception {
+        int nbTodos = todoDao.countByTodoList(todoList);
 
         mockMvc.perform(
-                delete("/todo-lists/2")
+                delete("/todo-lists/" + todoList.getNum() + "/todos/2")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .with(csrf()))
                 .andExpect(status().isFound())
-                .andExpect(view().name("redirect:/todo-lists"));
+                .andExpect(view().name("redirect:/todo-lists/{todoListNum}/todos"));
 
-        List<TodoList> todoLists = todoListDao.findAllByCustomer(customer);
-        assertEquals(nbTodoLists - 1, todoLists.size());
+        List<Todo> todos = todoDao.findAllByTodoList(todoList);
+        assertEquals(nbTodos - 1, todos.size());
     }
 
 }
