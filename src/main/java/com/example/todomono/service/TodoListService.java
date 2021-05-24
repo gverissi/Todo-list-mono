804e5732d28434d78ebb1f2dea0943c51823ec1f
gverissi <greg.verissimo@gmail.com>
2021-05-24 14:33:34 +0200

    Add JavaDoc for TodoListService

--- src/main/java/com/example/todomono/service/TodoListService.java
+++ src/main/java/com/example/todomono/service/TodoListService.java
@@ -21,6 +21,14 @@ public class TodoListService {
         this.todoListDao = todoListDao;
     }
 
+    /**
+     * Create a TodoList associated to a Customer. If the Customer already contains a TodoList with the same title
+     * then a EntityAlreadyExistException is thrown.
+     * @param customer Parent of the TodoList.
+     * @param todoListForm Dto that must contain a unique tile
+     * @return The created TodoList entity.
+     * @throws EntityAlreadyExistException If TodoList title is not unique.
+     */
     public TodoList createOneForCustomer(Customer customer, TodoListForm todoListForm) throws EntityAlreadyExistException {
         String title = todoListForm.getTitle();
         TodoList todoList = new TodoList(title);
@@ -33,14 +41,34 @@ public class TodoListService {
         }
     }
 
+    /**
+     * Find all TodoList own by a Customer. The TodoLists are ordered by num asc.
+     * @param customer Parent of the TodoList.
+     * @return List of TodoList.
+     */
     public List<TodoList> findAllByCustomer(Customer customer) {
         return todoListDao.findAllByCustomer(customer);
     }
 
+    /**
+     * Find one TodoList according to it's num and it's parent Customer.
+     * If the TodoList doesn't exit then Throw an DaoEntityNotFoundException that extend RuntimeException and the 404 error page is displayed.
+     * @param customer Parent of the TodoList.
+     * @param todoListNum Num of the TodoList.
+     * @return The TodoList entity.
+     */
     public TodoList findOneByCustomerAndNum(Customer customer, int todoListNum) {
         return todoListDao.findByCustomerAndNum(customer, todoListNum);
     }
 
+    /**
+     * Update a TodoList owned by a Customer, according to a TodoListForm Dto.
+     * If the Todo doesn't exist Throw an DaoEntityNotFoundException and thus the 404 error page is displayed.
+     * @param customer Parent of the TodoList.
+     * @param todoListForm Dto containing the modifications.
+     * @return The modified TodoList entity.
+     * @throws EntityAlreadyExistException If the modified title already exist.
+     */
     public TodoList updateOneForCustomer(Customer customer, TodoListForm todoListForm) throws EntityAlreadyExistException {
         String title = todoListForm.getTitle();
         int todoListNum = todoListForm.getNum();
@@ -54,12 +82,23 @@ public class TodoListService {
         }
     }
 
+    /**
+     * Delete a TodoList.
+     * If the TodoList doesn't exist Throw an DaoEntityNotFoundException and thus the 404 error page is displayed.
+     * @param customer Parent of the TodoList.
+     * @param todoListNum Num of the TodoList.
+     */
     public void deleteOneForCustomer(Customer customer, int todoListNum) {
         TodoList todoList = todoListDao.findByCustomerAndNum(customer, todoListNum);
         todoListDao.deleteById(todoList.getId());
         computeTodoListNum(customer);
     }
 
+    /**
+     * Compute all TodoLists num. This method must be called after deleting a Todo.
+     * This way the nums is a continuous series starting at 1.
+     * @param customer Parent of the TodoLists.
+     */
     private void computeTodoListNum(Customer customer) {
         int num = 0;
         List<TodoList> todoLists = todoListDao.findAllByCustomer(customer);
