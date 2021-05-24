7000ee5ee84d69f037f1ba42a5efb5f9803f33f4
gverissi <greg.verissimo@gmail.com>
2021-05-24 22:28:35 +0200

    Add "/" as route path for home in HomeController

--- src/main/java/com/example/todomono/controller/HomeController.java
+++ src/main/java/com/example/todomono/controller/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping({"/home", "/"})
 public class HomeController {
 
     private final HomeService homeService;
